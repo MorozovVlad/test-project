bc768ba9c57c5e7ca4faba9fc1630b07036fb207
vipo_1 <vipo_1>
2026-05-13 16:55:35 +0300

    Записываю в localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Todo from './components/Todo'
 import { Context } from './context/context'
@@ -8,11 +8,23 @@ function App() {
   const[tasks, setTasks] = useState([])
   const[selectedTask, setSelectedTask] = useState({title:"", time:"", isStart: false})
 
+  // useEffect(()=>{
+  //   const tasksLocal = localStorage.getItem('tasks')
+  //   setTasks(tasksLocal)
+  // }, [])
+
   function addTask(title, text, time){
-      const id = new Date()
+      const id = Date.now()
       const newTask = {id, title, text, time}
       setTasks((prev)=>[...prev, newTask])
+
+      let arr = JSON.parse(localStorage.getItem('tasks')) || []
+      console.log(arr)
+      arr.push(newTask)
+      localStorage.setItem("tasks", JSON.stringify(arr));
   }
+
+  
 
   function removeTask(idRemove){
     setTasks((prev)=>prev.filter((task, id)=> task.id!=idRemove))
